e1fff6683cb134546b3ddfff3ce5cea4b5745c2f
M. Dietrich <mdt@emdete.de>
2020-12-20 23:18:06 +0100

    dhcp finish

--- honey.py
+++ honey.py
@@ -38,42 +38,31 @@ if 0:
 	server_thread.join()
 
 if 1:
-	from dhcp_server import DHCP_Server
-	import threading
+	from l3 import LevelThree
 
-	class FakeServer(object):
-		def __init__(self, server_ip, interface):
-			self.dhcp_server = None
-			self.dhcp_server = DHCP_Server(server_ip, interface)
-			self.dhcp_server.set_server_name('free')
-			self.dhcp_server.set_dns('172.16.0.1')
-			self.dhcp_server.set_server_lease_time(60*30)
-			self.server_thread = threading.Thread(target=self.dhcp_server.start_server)
-			self.server_thread.daemon = True
-			self.server_thread.start()
+	class FakeDhcp(LevelThree):
+		def get_ip_for_mac(self, message_type, client_hardware_address, package):
+			return IPv4Address('172.16.66.111')
 
-		def __del__(self):
-			if self.dhcp_server:
-				self.dhcp_server.stop_server()
-
-		def get_ip_for_mac(self, mac):
-			return '172.16.66.22'
-
-
-try:
-	interface = 'eth0'
-	net_bits = 24
-	network = IPv4Network('172.16.66.0/{}'.format(net_bits)) # define the network we are in
-	server_ip = next(network.hosts()) # take the first one
-	if call('ip l set dev {} up'.format(interface), shell=True):
-		raise Exception()
-	if call('ip a add dev {} {}/{}'.format(interface, server_ip, net_bits), shell=True):
-		raise Exception()
-	fake_dhcp = FakeServer(server_ip, interface)
-	fake_dhcp.server_thread.join()
-finally:
-	if call('ip a del dev {} {}/{}'.format(interface, server_ip, net_bits), shell=True):
-		raise Exception()
-#	if call('ip l set dev {} down'.format(interface), shell=True):
-#		raise Exception()
+	try:
+		interface = 'eth0'
+		net_bits = 24
+		network = IPv4Network('172.16.66.0/{}'.format(net_bits)) # define the network we are in
+		server_ip_address = next(network.hosts()) # take the first one
+		if call('ip l set dev {} up'.format(interface), shell=True):
+			raise Exception()
+		if call('ip a add dev {} {}/{}'.format(interface, server_ip_address, net_bits), shell=True):
+			raise Exception()
+		FakeDhcp(
+			interface=interface,
+			server_ip_address=server_ip_address,
+			subnet_mask=network.netmask,
+			broadcast_address=network.broadcast_address,
+			lease_time=60*30,
+			).run()
+	finally:
+		if call('ip a del dev {} {}/{}'.format(interface, server_ip_address, net_bits), shell=True):
+			raise Exception()
+	#	if call('ip l set dev {} down'.format(interface), shell=True):
+	#		raise Exception()
 
